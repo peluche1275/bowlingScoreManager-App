98f1dce186a9f65e7d190aae4f241741504a3b4f
Nathan <nathan.altomare.belier@gmail.com>
2021-03-12 19:40:27 +0100

    Some work

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const { run } = require("./dbconnection");
+const { dataBaseManager } = require("./dbconnection");
 const app = express();
 
-run();
+async function test() {
+    await dataBaseManager.connectionToTheDatabase();
+    await dataBaseManager.searchTheDashboard();
+}
+
+test()
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render("index")
 })
+
 app.listen(process.env.PORT || 8080)
--- dbconnection.js
+++ dbconnection.js
@@ -3,13 +3,23 @@ const { uri } = require("./uri");
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-async function run() {
+async function connectionToTheDatabase() {
     try {
         await client.connect();
-        await client.db("bowling").command({ ping: 1 });
         console.log("Connected successfully to server");
     } catch (error) {
         console.log("Erreur !");
     }
 }
-exports.run = run;
+
+async function searchTheDashboard() {
+    let array = []
+    const database = client.db("bowling");
+    const dashboard = database.collection("dashboard");
+    await dashboard.find().forEach(function(doc) {
+        array.push({ name: doc.name, score: doc.score })
+    });
+    return array
+}
+
+exports.dataBaseManager = { connectionToTheDatabase, searchTheDashboard };
